7be1d0d854f9210274ee16185802d5693621de13
Souheil CHELFOUH <trollfot@gmail.com>
2009-09-18 14:34:07 +0000

    Renaming viewlets to remove the "admin" prefix, totally useless. Added static files.

--- src/grokui/base/layout.py
+++ src/grokui/base/layout.py
@@ -3,6 +3,8 @@ import megrok.menu
 from megrok.layout import Layout, Page
 from zope.app.folder.interfaces import IRootFolder
 
+grok.templatedir("templates")
+
 
 class AdminLayout(Layout):
     """The general layout for the administration
--- src/grokui/base/viewlets.py
+++ src/grokui/base/viewlets.py
@@ -1,51 +1,30 @@
 # -*- coding: utf-8 -*-
 
 import grok
-from zope.app.folder.interfaces import IRootFolder
-from grokui.base.contentproviders import AdministrationHeader, AdministrationFooter
+
 from zope.component import getUtility
+from zope.app.folder.interfaces import IRootFolder
 from zope.traversing.browser.absoluteurl import absoluteURL
 from zope.app.publisher.interfaces.browser import IBrowserMenu
 from zope.app.security.interfaces import IUnauthenticatedPrincipal
+
+from z3c.flashmessage.interfaces import IMessageReceiver
+from grokui.base.contentproviders import AdministrationHeader
+from grokui.base.contentproviders import AdministrationFooter
 
 grok.context(IRootFolder)
 grok.templatedir("templates")
 grok.viewletmanager(AdministrationHeader)
 
 
-class AdministrationMenuViewlet(grok.Viewlet):
-    grok.order(40)
-    grok.name("admin_menu")
-
-    def update(self):
-        self.contexturl = absoluteURL(self.context, self.request)
-        menu = getUtility(IBrowserMenu, 'grokui_admin_menu')
-        self.actions = menu.getMenuItems(self.context, self.request)
-    
-
-class AdministrationBanner(grok.Viewlet):
+class Banner(grok.Viewlet):
     grok.order(10)
-    grok.name('admin_banner')
-
-    
-class GrokRelax(grok.Viewlet):
-    grok.order(20)
-    grok.name('admin_decoration')
-
-
-class Messages(grok.Viewlet):
-    grok.order(40)
-    grok.name('admin_messages')
-
-    @property
-    def messages(self):
-        receiver = getUtility(IMessageReceiver)
-        return receiver.receive()
+    grok.name('grokui.banner')
 
 
 class LoginInformation(grok.Viewlet):
-    grok.order(30)
-    grok.name('admin_login')
+    grok.order(20)
+    grok.name('grokui.login')
 
     @property
     def is_authenticated(self):
@@ -53,8 +32,28 @@ class LoginInformation(grok.Viewlet):
         """
         return not IUnauthenticatedPrincipal.providedBy(self.request.principal)
 
-    
-class AdministrationAuthors(grok.Viewlet):
+
+class MenuViewlet(grok.Viewlet):
+    grok.order(30)
+    grok.name("grokui.menu")
+
+    def update(self):
+        self.contexturl = absoluteURL(self.context, self.request)
+        menu = getUtility(IBrowserMenu, 'grokui_admin_menu')
+        self.actions = menu.getMenuItems(self.context, self.request)
+
+
+class Messages(grok.Viewlet):
+    grok.order(40)
+    grok.name('grokui.messages')
+
+    @property
+    def messages(self):
+        receiver = getUtility(IMessageReceiver)
+        return receiver.receive()
+
+
+class Authors(grok.Viewlet):
     grok.order(10)
-    grok.name('admin_authors')
+    grok.name('grokui.authors')
     grok.viewletmanager(AdministrationFooter)
