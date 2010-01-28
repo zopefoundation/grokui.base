34a4dc81cf1e40bf3e1907a2b54f599cca30ac35
Souheil CHELFOUH <trollfot@gmail.com>
2010-01-28 12:12:59 +0000

    Added url and flash.

--- src/grokui/base/layout.py
+++ src/grokui/base/layout.py
@@ -2,9 +2,13 @@
 
 import grok
 import megrok.menu
-from megrok.layout import Layout, Page
-from grokui.base import IGrokUIRealm, GrokUILayer, IUIPanel, MainMenu
+import z3c.flashmessage.interfaces
 
+from grok import util
+from grokui.base import IGrokUIRealm, GrokUILayer, IUIPanel, MainMenu
+from megrok.layout import Layout, Page
+from zope.component import getUtility
+from zope.traversing.browser.absoluteurl import absoluteURL
 
 grok.layer(GrokUILayer)
 grok.templatedir("templates")
@@ -16,6 +20,9 @@ class GrokUILayout(Layout):
     grok.context(IGrokUIRealm)
     title = u"Grok User Interface"
 
+    def update(self):
+        self.baseurl = absoluteURL(self.context, self.request) + '/'
+        
 
 class GrokUIView(Page):
     """A grok ui view.
@@ -24,3 +31,11 @@ class GrokUIView(Page):
     grok.context(IGrokUIRealm)
     grok.implements(IUIPanel)
     megrok.menu.menuitem(MainMenu)
+
+    def application_url(self, name=None, data=None):
+        return util.application_url(self.request, self.context, name, data)
+
+    def flash(self, message, type='message'):
+        source = getUtility(
+            z3c.flashmessage.interfaces.IMessageSource, name='session')
+        source.send(message, type)
