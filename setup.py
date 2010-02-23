555c5b9a469d398f4c7dcb1712efab100e5a706a
Uli Fouquet <uli@gnufix.de>
2010-02-23 12:58:23 +0000

    Back to development: 0.2.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(name='grokui.base',
-      version='0.1dev',
+      version='0.2dev',
       description="The Grok administration and development UI (base)",
       long_description=(
         read('README.txt') +
