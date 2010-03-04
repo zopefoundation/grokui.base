8589729a3863bb8d9440255dd9fd6da282daf4fb
Christian Klinger <cklinger@novareto.de>
2010-03-04 10:04:23 +0000

    removed last flashmessage deps

--- src/grokui/base/messages.py
+++ src/grokui/base/messages.py
@@ -1,29 +1,9 @@
 # -*- coding: utf-8 -*-
 
 import grok
-from z3c.flashmessage.message import PersistentMessage
-from z3c.flashmessage.interfaces import IMessageSource
+from grokcore.message import UniqueMessageSource
 
-
-class AdminMessageSource(grok.GlobalUtility):
+class AdminMessageSource(UniqueMessageSource):
     """Source for the administration messages
     """
     grok.name('admin')
-    grok.implements(IMessageSource)
-
-    message = None
-
-    def send(self, message, type='admin'):
-        self.message = PersistentMessage(message, type)
-
-    def list(self, type=None):
-        if self.message is None:
-            return
-        if type is None or self.message.type == type:
-            yield self.message
-
-    def delete(self, message):
-        if message is self.message:
-            self.message = None
-        else:
-            raise KeyError(message)
