f24a231fb8f5fa842a3c1472c15bbb15a4836cf9
Jan-Wijbrand Kolman <janwijbrand@gmail.com>
2010-11-03 16:23:32 +0000

    Back to development: 0.4.2

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(name='grokui.base',
-      version='0.4.1',
+      version='0.4.2dev',
       description="The Grok administration and development UI (base)",
       long_description=(
         read('README.txt') +
