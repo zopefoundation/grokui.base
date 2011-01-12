7109750d4361299c3f13434c3dc2bfced22502b7
Jan-Jaap Driessen <janjaapdriessen+github@gmail.com>
2011-01-12 15:39:34 +0000

    Preparing release 0.5

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(name='grokui.base',
-      version='0.4.2',
+      version='0.5',
       description="The Grok administration and development UI (base)",
       long_description=(
         read('README.txt') +
