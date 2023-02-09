4337eeba26f304473f4675b0b102ba162982f234
Michael Howitz <mh@gocept.com>
2023-02-09 15:42:55 +0100

    Drop support for Python 2.7 up to 3.6.

--- src/grokui/base/__init__.py
+++ src/grokui/base/__init__.py
@@ -1,11 +1,13 @@
-from grokui.base.contentproviders import Footer
-from grokui.base.contentproviders import Header
-from grokui.base.contentproviders import MainMenu
-from grokui.base.contentproviders import Messages
 from grokui.base.interfaces import IGrokUIRealm
 from grokui.base.interfaces import IMainMenu
 from grokui.base.interfaces import IUIPanel
-from grokui.base.layout import GrokUIView
 from grokui.base.namespace import GrokUILayer
 from grokui.base.namespace import GrokUINamespace
 from grokui.base.namespace import GrokUISkin
+
+
+from grokui.base.contentproviders import Footer  # isort: skip
+from grokui.base.contentproviders import Header  # isort: skip
+from grokui.base.contentproviders import MainMenu  # isort: skip
+from grokui.base.contentproviders import Messages  # isort: skip
+from grokui.base.layout import GrokUIView  # isort: skip
